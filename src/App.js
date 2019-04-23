732a37cfd52923989e9cba6d6f2a6effecd6825c
vagrant <vagrant@vagrant.vm>
2019-04-23 12:35:06 +0000

    learn state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 
 // class App extends Component {
 //   render() {
@@ -11,28 +10,34 @@ import PropTypes from 'prop-types';
 //   )
 // }
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10},
-    { name: "Hanako", age: 8},
-    { name: "Hanako", age: 8}
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
-}
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age}years old!</div>
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  // constructorは初期化処理が実行される
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  
+  handlePlusButton = () => {
+    // setStateは状態を変更するときの決まり文句
+    this.setState({ count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    // setStateは状態を変更するときの決まり文句
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      <div>count: { this.state.count }</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
 export default App;
